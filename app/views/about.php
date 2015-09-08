7b3a40cd3814cab4a8b620a57b6603ff4b6965eb
Hamish <hamish@methodstudios.co.nz>
2015-09-08 19:49:31 +1200

    Fallback Images for map

--- app/views/about.php
+++ app/views/about.php
@@ -9,15 +9,51 @@
 <div class="about--wrap">
 
     <section class="about--shoot-locations">
-        <p class="font-large yellow half-block nav--left">
+        <p class="font-large font-black-italic yellow half-block nav--left">
             Shoot locations<br>
             across the globe
         </p>
         <div class="map-wrap">
-            <img src="<?php echo IMAGES_PATH; ?>layout/world-map.svg" alt="Shoot Locations - Please use a browser that supports SVG" />
+            <img class="map-base" src="<?php echo IMAGES_PATH; ?>layout/world-map.svg" alt="Shoot Locations - Please use a browser that supports SVG" />
             <div class="map-locations">
                 <span class="location loc-1"></span>
+                <span class="location loc-2"></span>
+                <span class="location loc-3"></span>
+                <span class="location loc-4"></span>
+                <span class="location loc-5"></span>
+                <span class="location loc-6"></span>
+                <span class="location loc-7"></span>
+                <span class="location loc-8"></span>
+                <span class="location loc-9"></span>
+                <span class="location loc-10"></span>
+                <span class="location loc-11"></span>
+                <span class="location loc-12"></span>
+                <span class="location loc-13"></span>
+                <span class="location loc-14"></span>
+                <span class="location loc-15"></span>
+                <span class="location loc-16"></span>
+                <span class="location loc-17"></span>
+                <span class="location loc-18"></span>
+                <span class="location loc-19"></span>
+                <span class="location loc-20"></span>
+                <span class="location loc-21"></span>
+                <span class="location loc-22"></span>
+                <span class="location loc-23"></span>
+                <span class="location loc-24"></span>
+                <span class="location loc-25"></span>
+                <span class="location loc-26"></span>
+                <span class="location loc-27"></span>
+                <span class="location loc-28"></span>
+                <span class="location loc-29"></span>
+                <span class="location loc-30"></span>
+                <!-- <span class="location loc-31"></span>
+                <span class="location loc-32"></span>
+                <span class="location loc-33"></span>
+                <span class="location loc-34"></span>
+                <span class="location loc-35"></span> -->
             </div>
+            <!-- <img class="map-base-img" src="<?php echo IMAGES_PATH; ?>layout/map-base.jpg" alt="World Map" />
+            <img class="map-locations-img" src="<?php echo IMAGES_PATH; ?>layout/map-locations.png" alt="Shoot Locations" /> -->
         </div>
     </section>
 
